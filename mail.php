184d6bd72ceb063af9f7963a8ec2794ded7c61ec
dasinua <dasinua@gmail.com>
2016-07-05 14:21:08 +0300

    first commit

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+
+$recepient = "dasflancer@gmail.com";
+$sitename = "Юридическая фирма 'Вершинин и партнеры'";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$email = trim($_POST["email"]);
+$text = trim($_POST["text"]);
+$message = "Имя: $name \nТелефон: $phone \nE-mail: $email \nТекст: $text";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
